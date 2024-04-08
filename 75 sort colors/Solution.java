fee1ac3973b9cc09eefc085f4e0258338b74beb2
Viram Shah <132284751+viramhh98@users.noreply.github.com>
2024-04-08 23:49:14 +0530

    day 2

---
+++ 75 sort colors/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        int mid=0;
+        for(int i=0; mid<=high; i++){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
